7a72a52b39230f0fd100638444ec620863884da9
phynos <phynos@126.com>
2019-09-26 19:48:41 +0800

    1

--- src/phynos-web/phynos-api/src/main/java/com/phynos/framework/web/api/controller/IndexController.java
+++ src/phynos-web/phynos-api/src/main/java/com/phynos/framework/web/api/controller/IndexController.java
@@ -1,8 +1,14 @@
 package com.phynos.framework.web.api.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * @author by Lupc
  * @date 2019/9/26.
  */
+@RestController
 public class IndexController {
+
+
+
 }
