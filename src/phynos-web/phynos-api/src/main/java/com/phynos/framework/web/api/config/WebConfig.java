4ae8e9d06452814ecc0a5ef86284243b6418fd75
phynos <phynos@126.com>
2019-10-18 20:14:38 +0800

    1

--- src/phynos-web/phynos-api/src/main/java/com/phynos/framework/web/api/config/WebConfig.java
+++ src/phynos-web/phynos-api/src/main/java/com/phynos/framework/web/api/config/WebConfig.java
@@ -1,8 +1,37 @@
 package com.phynos.framework.web.api.config;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
 /**
  * @author by Lupc
  * @date 2019/9/26.
  */
-public class WebConfig {
+@Configuration
+public class WebConfig extends WebMvcConfigurationSupport {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        //配置
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+        //配置 （jar内置）资源文件访问
+        registry.addResourceHandler("/**")
+                .addResourceLocations("classpath:/static/");
+        //配置 上传文件访问路径
+        registry.addResourceHandler("/upload/**")
+                .addResourceLocations("file:static/upload/");
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedOrigins("*")
+                .allowedMethods("GET", "HEAD", "POST","PUT", "DELETE", "OPTIONS")
+                .allowCredentials(true).maxAge(3600);
+    }
+
 }
