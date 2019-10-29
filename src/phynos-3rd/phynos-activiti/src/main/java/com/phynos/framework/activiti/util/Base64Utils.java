ef124c85bdcd9819e874597a9589b3cbf4eb98b9
phynos <phynos@126.com>
2019-10-29 14:20:48 +0800

    11

--- src/phynos-3rd/phynos-activiti/src/main/java/com/phynos/framework/activiti/util/Base64Utils.java
+++ src/phynos-3rd/phynos-activiti/src/main/java/com/phynos/framework/activiti/util/Base64Utils.java
@@ -3,20 +3,17 @@ package com.phynos.framework.activiti.util;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import sun.misc.BASE64Decoder;
-import sun.misc.BASE64Encoder;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Base64;
 
 /**
  * Created by user on 2018/7/12.
  */
 public class Base64Utils {
+
     static Logger logger = LoggerFactory.getLogger(Base64Utils.class);
-    private static BASE64Encoder encoder = new BASE64Encoder();
 
     public static String ioToBase64(InputStream in) throws IOException {
         String strBase64 = null;
@@ -24,11 +21,12 @@ public class Base64Utils {
             byte[] bytes = new byte[in.available()];
             // 将文件中的内容读入到数组中
             in.read(bytes);
-            strBase64 = encoder.encode(bytes);      //将字节流数组转换为字符串
+            strBase64 = Base64.getEncoder().encodeToString(bytes);
             in.close();
         } catch (IOException ioe) {
             logger.error("图片转64编码异常",ioe);
         }
         return strBase64;
     }
+
 }
