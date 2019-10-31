ddd61b3b887920f3898e0b78ca3e42def287747d
phynos <phynos@126.com>
2019-10-31 09:37:16 +0800

    1

---
+++ src/phynos-web/phynos-api/src/main/java/com/phynos/framework/web/api/controller/system/DictController.java
@@ -0,0 +1,14 @@
+package com.phynos.framework.web.api.controller.system;
+
+import com.phynos.framework.web.api.controller.BaseController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @Author: Lupc
+ * @Date: 2019/10/31 9:34
+ **/
+@RequestMapping("/api/sys/dict")
+public class DictController extends BaseController  {
+
+
+}
