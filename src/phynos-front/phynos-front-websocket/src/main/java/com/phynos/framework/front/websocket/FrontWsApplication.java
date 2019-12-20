159912b9b2856686d9d2b73501947c15f1609902
phynos <phynos@126.com>
2019-12-20 16:46:33 +0800

    12

---
+++ src/phynos-front/phynos-front-websocket/src/main/java/com/phynos/framework/front/websocket/FrontWsApplication.java
@@ -0,0 +1,17 @@
+package com.phynos.framework.front.websocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author by Lupc
+ * @date 2019/9/24.
+ */
+@SpringBootApplication
+public class FrontWsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FrontWsApplication.class,args);
+    }
+
+}
--- src/phynos-front/phynos-front-websocket/src/main/java/com/phynos/framework/front/websocket/controller/WebSocketController.java
+++
@@ -1,8 +0,0 @@
-package com.phynos.framework.front.websocket.controller;
-
-/**
- * @author by Lupc
- * @date 2019/9/24.
- */
-public class WebSocketController {
-}
