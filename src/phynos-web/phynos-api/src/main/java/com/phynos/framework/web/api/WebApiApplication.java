f712b411c42c20a6ec4adc75a4284940bd505aaa
phynos <phynos@126.com>
2020-08-21 12:00:40 +0800

    简化本项目，改为简单的单体应用

--- src/phynos-web/phynos-api/src/main/java/com/phynos/framework/web/api/WebApiApplication.java
+++ src/phynos-web/phynos-api/src/main/java/com/phynos/framework/web/api/WebApiApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 /**
@@ -14,6 +15,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @SpringBootApplication
 @EnableTransactionManagement
 @EnableCaching
+@EnableAsync
 @MapperScan({"com.phynos.framework.dao.mapper"})
 @ComponentScan({
         "com.phynos.framework.web.api",
---
+++ src/phynos-web/phynos-api/src/main/java/com/phynos/framework/web/api/controller/device/DeviceController.java
@@ -0,0 +1,8 @@
+package com.phynos.framework.web.api.controller.device;
+
+/**
+ * @author by lupc
+ * @date 2020-08-21 11:55
+ */
+public class DeviceController {
+}
