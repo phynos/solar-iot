bc113bffae9adb11e4c5850dca1dd8557bbbf57b
phynos <phynos@126.com>
2020-09-29 15:16:12 +0800

    1

--- src/phynos-front/phynos-front-mqtt/src/main/java/com/phynos/framework/front/mqtt/SimpleMqttClient.java
+++ src/phynos-front/phynos-front-mqtt/src/main/java/com/phynos/framework/front/mqtt/SimpleMqttClient.java
@@ -5,145 +5,129 @@ import org.eclipse.paho.client.mqttv3.*;
 
 public class SimpleMqttClient implements MqttCallback {
 
-	MqttClient myClient;
-	MqttConnectOptions connOpt;
+    MqttClient myClient;
+    MqttConnectOptions connOpt;
 
-	static final String BROKER_URL = "tcp://127.0.0.1:61613";
-	static final String M2MIO_DOMAIN = "<Insert m2m.io domain here>";
-	static final String M2MIO_STUFF = "things";
-	static final String M2MIO_THING = "<Unique device ID>";
-	static final String M2MIO_USERNAME = "admin";
-	static final String M2MIO_PASSWORD_MD5 = "password";
+    static final String BROKER_URL = "tcp://172.16.22.161:1883";
+    static final String M2MIO_DOMAIN = "m2m.phynos.com";
+    static final String M2MIO_STUFF = "things";
+    static final String M2MIO_THING = "007";
+    static final String M2MIO_USERNAME = "admin";
+    static final String M2MIO_PASSWORD_MD5 = "123456";
 
-	// the following two flags control whether this example is a publisher, a subscriber or both
-	static final Boolean subscriber = true;
-	static final Boolean publisher = true;
+    // the following two flags control whether this example is a publisher, a subscriber or both
+    static final Boolean subscriber = true;
+    static final Boolean publisher = true;
 
-	/**
-	 * 
-	 * connectionLost
-	 * This callback is invoked upon losing the MQTT connection.
-	 * 
-	 */
-	@Override
-	public void connectionLost(Throwable t) {
-		System.out.println("Connection lost!");
-		// code to reconnect to the broker would go here if desired
-	}
-	
-	@Override
-	public void deliveryComplete(IMqttDeliveryToken token) {
-		// TODO Auto-generated method stub
-		
-	}
+    /**
+     * connectionLost
+     * This callback is invoked upon losing the MQTT connection.
+     */
+    @Override
+    public void connectionLost(Throwable t) {
+        System.out.println("Connection lost!");
+        // code to reconnect to the broker would go here if desired
+    }
 
-	@Override
-	public void messageArrived(String topic, MqttMessage message) throws Exception {
-		System.out.println("-------------------------------------------------");
-		System.out.println("| Topic:" + topic);
-		System.out.println("| Message: " + new String(message.getPayload()));
-		System.out.println("-------------------------------------------------");		
-	}
+    @Override
+    public void deliveryComplete(IMqttDeliveryToken token) {
+        // TODO Auto-generated method stub
 
-	/**
-	 * 
-	 * messageArrived
-	 * This callback is invoked when a message is received on a subscribed topic.
-	 * 
-	 */
-	public void messageArrived(MqttTopic topic, MqttMessage message) throws Exception {
-		
-	}
+    }
 
-	/**
-	 * 
-	 * MAIN
-	 * 
-	 */
-	public static void main(String[] args) {
-		SimpleMqttClient smc = new SimpleMqttClient();
-		smc.runClient();
-	}
+    @Override
+    public void messageArrived(String topic, MqttMessage message) throws Exception {
+        System.out.println("-------------------------------------------------");
+        System.out.println("| Topic:" + topic);
+        System.out.println("| Message: " + new String(message.getPayload()));
+        System.out.println("-------------------------------------------------");
+    }
 
-	/**
-	 * 
-	 * runClient
-	 * The main functionality of this simple example.
-	 * Create a MQTT client, connect to broker, pub/sub, disconnect.
-	 * 
-	 */
-	public void runClient() {
-		// setup MQTT Client
-		String clientID = M2MIO_THING;
-		connOpt = new MqttConnectOptions();
+    /**
+     * messageArrived
+     * This callback is invoked when a message is received on a subscribed topic.
+     */
+    public void messageArrived(MqttTopic topic, MqttMessage message) throws Exception {
 
-		connOpt.setCleanSession(true);
-		connOpt.setKeepAliveInterval(30);
-		connOpt.setUserName(M2MIO_USERNAME);
-		connOpt.setPassword(M2MIO_PASSWORD_MD5.toCharArray());
+    }
 
-		// Connect to Broker
-		try {
-			myClient = new MqttClient(BROKER_URL, clientID);
-			myClient.setCallback(this);
-			myClient.connect(connOpt);
-		} catch (MqttException e) {
-			e.printStackTrace();
-			System.exit(-1);
-		}
+    /**
+     * runClient
+     * The main functionality of this simple example.
+     * Create a MQTT client, connect to broker, pub/sub, disconnect.
+     */
+    public void runClient() {
+        // setup MQTT Client
+        String clientID = M2MIO_THING;
+        connOpt = new MqttConnectOptions();
 
-		System.out.println("Connected to " + BROKER_URL);
+        connOpt.setCleanSession(true);
+        connOpt.setKeepAliveInterval(30);
+        connOpt.setUserName(M2MIO_USERNAME);
+        connOpt.setPassword(M2MIO_PASSWORD_MD5.toCharArray());
 
-		// setup topic
-		// topics on m2m.io are in the form <domain>/<stuff>/<thing>
-		String myTopic = M2MIO_DOMAIN + "/" + M2MIO_STUFF + "/" + M2MIO_THING;
-		MqttTopic topic = myClient.getTopic(myTopic);
+        // Connect to Broker
+        try {
+            myClient = new MqttClient(BROKER_URL, clientID);
+            myClient.setCallback(this);
+            myClient.connect(connOpt);
+        } catch (MqttException e) {
+            e.printStackTrace();
+            System.exit(-1);
+        }
 
-		// subscribe to topic if subscriber
-		if (subscriber) {
-			try {
-				int subQoS = 0;
-				myClient.subscribe(myTopic, subQoS);
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		}
+        System.out.println("Connected to " + BROKER_URL);
 
-		// publish messages if publisher
-		if (publisher) {
-			for (int i=1; i<=10; i++) {
-				String pubMsg = "{\"pubmsg\":" + i + "}";
-				int pubQoS = 0;
-				MqttMessage message = new MqttMessage(pubMsg.getBytes());
-				message.setQos(pubQoS);
-				message.setRetained(false);
+        // setup topic
+        // topics on m2m.io are in the form <domain>/<stuff>/<thing>
+        String myTopic = M2MIO_DOMAIN + "/" + M2MIO_STUFF + "/" + M2MIO_THING;
+        MqttTopic topic = myClient.getTopic(myTopic);
 
-				// Publish the message
-				System.out.println("Publishing to topic \"" + topic + "\" qos " + pubQoS);
-				MqttDeliveryToken token = null;
-				try {
-					// publish message to broker
-					token = topic.publish(message);
-					// Wait until the message has been delivered to the broker
-					token.waitForCompletion();
-					Thread.sleep(100);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}			
-		}
+        // subscribe to topic if subscriber
+        if (subscriber) {
+            try {
+                int subQoS = 0;
+                myClient.subscribe(myTopic, subQoS);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
 
-		// disconnect
-		try {
-			// wait to ensure subscribed messages are delivered
-			if (subscriber) {
-				Thread.sleep(5000);
-			}
-			myClient.disconnect();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+        // publish messages if publisher
+        if (publisher) {
+            for (int i = 1; i <= 10; i++) {
+                String pubMsg = "{\"pubmsg\":" + i + "}";
+                int pubQoS = 0;
+                MqttMessage message = new MqttMessage(pubMsg.getBytes());
+                message.setQos(pubQoS);
+                message.setRetained(false);
+
+                // Publish the message
+                System.out.println("Publishing to topic \"" + topic + "\" qos " + pubQoS);
+                MqttDeliveryToken token = null;
+                try {
+                    // publish message to broker
+                    token = topic.publish(message);
+                    // Wait until the message has been delivered to the broker
+                    token.waitForCompletion();
+                    Thread.sleep(100);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        // disconnect
+        try {
+            // wait to ensure subscribed messages are delivered
+            if (subscriber) {
+                Thread.sleep(5000);
+            }
+            myClient.disconnect();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
 
 }
