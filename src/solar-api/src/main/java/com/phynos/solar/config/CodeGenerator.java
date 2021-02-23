952bb47c9e11a4204eb71606922332edfdf91dd2
phynos <phynos@126.com>
2021-02-23 10:06:30 +0800

    1

---
+++ src/solar-api/src/main/java/com/phynos/solar/config/CodeGenerator.java
@@ -0,0 +1,139 @@
+package com.phynos.solar.config;
+
+import com.baomidou.mybatisplus.core.exceptions.MybatisPlusException;
+import com.baomidou.mybatisplus.generator.AutoGenerator;
+import com.baomidou.mybatisplus.generator.InjectionConfig;
+import com.baomidou.mybatisplus.generator.config.*;
+import com.baomidou.mybatisplus.generator.config.rules.NamingStrategy;
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.Scanner;
+
+/**
+ * mybatis-plus 代码生成
+ *
+ * @author by lupc
+ * @date 2021-01-25 9:52
+ */
+public class CodeGenerator {
+
+    /**
+     * <p>
+     * 读取控制台内容
+     * </p>
+     */
+    public static String scanner(String tip) {
+        Scanner scanner = new Scanner(System.in);
+        StringBuilder help = new StringBuilder();
+        help.append("请输入" + tip + "：");
+        System.out.println(help.toString());
+        if (scanner.hasNext()) {
+            String ipt = scanner.next();
+            if (StringUtils.isNotBlank(ipt)) {
+                return ipt;
+            }
+        }
+        throw new MybatisPlusException("请输入正确的" + tip + "！");
+    }
+
+    public static void main(String[] args) {
+        // 代码生成器
+        AutoGenerator mpg = new AutoGenerator();
+
+        // 全局配置
+        GlobalConfig gc = new GlobalConfig();
+        String projectPath = System.getProperty("user.dir");
+        gc.setOutputDir(projectPath + "/src/main/java");
+        gc.setAuthor("lupc");
+        gc.setOpen(false);
+        mpg.setGlobalConfig(gc);
+
+        // 数据源配置
+        DataSourceConfig dsc = new DataSourceConfig();
+        dsc.setUrl("jdbc:postgresql://172.16.22.161:5432/mesh?useUnicode=true&characterEncoding=utf-8");
+        dsc.setSchemaName("public");
+        dsc.setDriverName("org.postgresql.Driver");
+        dsc.setUsername("postgres");
+        dsc.setPassword("postgres");
+        mpg.setDataSource(dsc);
+
+        // 包配置
+        PackageConfig pc = new PackageConfig();
+        pc.setModuleName(scanner("模块名"));
+        pc.setParent("com.phynos.solar");
+        mpg.setPackageInfo(pc);
+
+        // 自定义配置
+        InjectionConfig cfg = new InjectionConfig() {
+            @Override
+            public void initMap() {
+                // to do nothing
+            }
+        };
+
+        // 如果模板引擎是 freemarker
+//        String templatePath = "/templates/mapper.xml.ftl";
+        // 如果模板引擎是 velocity
+        // String templatePath = "/templates/mapper.xml.vm";
+
+        // 自定义输出配置
+//        List<FileOutConfig> focList = new ArrayList<>();
+        // 自定义配置会被优先输出
+//        focList.add(new FileOutConfig(templatePath) {
+//            @Override
+//            public String outputFile(TableInfo tableInfo) {
+//                // 自定义输出文件名 ， 如果你 Entity 设置了前后缀、此处注意 xml 的名称会跟着发生变化！！
+//                return projectPath + "/src/main/resources/mapper/" + pc.getModuleName()
+//                        + "/" + tableInfo.getEntityName() + "Mapper" + StringPool.DOT_XML;
+//            }
+//        });
+        /*
+        cfg.setFileCreate(new IFileCreate() {
+            @Override
+            public boolean isCreate(ConfigBuilder configBuilder, FileType fileType, String filePath) {
+                // 判断自定义文件夹是否需要创建
+                checkDir("调用默认方法创建的目录，自定义目录用");
+                if (fileType == FileType.MAPPER) {
+                    // 已经生成 mapper 文件判断存在，不想重新生成返回 false
+                    return !new File(filePath).exists();
+                }
+                // 允许生成模板文件
+                return true;
+            }
+        });
+        */
+//        cfg.setFileOutConfigList(focList);
+        mpg.setCfg(cfg);
+
+        // 配置模板
+        TemplateConfig templateConfig = new TemplateConfig();
+        // 配置自定义输出模板
+        //指定自定义模板路径，注意不要带上.ftl/.vm, 会根据使用的模板引擎自动识别
+        // templateConfig.setEntity("templates/entity2.java");
+        // templateConfig.setService();
+        // templateConfig.setController();
+        templateConfig.setXml(null);
+        mpg.setTemplate(templateConfig);
+
+        // 策略配置
+        StrategyConfig strategy = new StrategyConfig();
+        strategy.setNaming(NamingStrategy.underline_to_camel);
+        strategy.setColumnNaming(NamingStrategy.underline_to_camel);
+        //strategy.setSuperEntityClass("你自己的父类实体,没有就不用设置!");
+        strategy.setEntityLombokModel(true);
+        strategy.setRestControllerStyle(true);
+        // 公共父类
+        //strategy.setSuperControllerClass("你自己的父类控制器,没有就不用设置!");
+        // 写于父类中的公共字段
+        strategy.setSuperEntityColumns("id");
+        strategy.setInclude(scanner("表名，多个英文逗号分割").split(","));
+        strategy.setControllerMappingHyphenStyle(true);
+        strategy.setTablePrefix(pc.getModuleName() + "_");
+        mpg.setStrategy(strategy);
+        //mpg.setTemplateEngine(new FreemarkerTemplateEngine());
+        mpg.execute();
+    }
+
+
+}
+
--- src/solar-api/src/main/java/com/phynos/solar/util/json/R.java
+++ src/solar-api/src/main/java/com/phynos/solar/util/json/R.java
@@ -6,6 +6,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 /**
+ * ajax返回结果包装类
  * @author by lupc
  * @date 2021-01-29 10:55
  */
@@ -13,11 +14,19 @@ import lombok.Setter;
 @Getter
 public class R<T> {
 
+    /**
+     * 状态值
+     */
     private int status;
 
+    /**
+     * 返回消息，可直接显示给用户看
+     */
     private String msg;
 
-    //这个字段用来开发阶段传递一些错误信息给前端，方便调试（不必定义过多的错误码）
+    /**
+     * 这个字段用来开发阶段传递一些错误信息给前端，方便调试（不必定义过多的错误码）
+     */
     @JsonInclude(JsonInclude.Include.NON_NULL)
     private String tip;
 
@@ -39,16 +48,35 @@ public class R<T> {
         return r;
     }
 
+    /**
+     * 自定义返回msg
+     * @param error
+     * @param msg
+     * @return
+     */
     public static R<?> msg(ResultCodeEnum error, String msg) {
         return msg(error, msg, null);
     }
 
+    /**
+     * 自定义返回msg，并附带tip
+     * @param error
+     * @param msg
+     * @param tip
+     * @return
+     */
     public static R<?> msg(ResultCodeEnum error, String msg, String tip) {
         R<?> r = new R<>(error.getCode(), msg);
         r.setTip(tip);
         return r;
     }
 
+    /**
+     * 附带调试信息
+     * @param error
+     * @param tip 调试信息
+     * @return
+     */
     public static R<?> tip(ResultCodeEnum error, String tip) {
         R<?> r = code(error);
         r.setTip(tip);
