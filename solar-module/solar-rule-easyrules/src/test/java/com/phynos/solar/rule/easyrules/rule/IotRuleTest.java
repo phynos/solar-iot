99e16372d80dac3237132bb1699a303c2679c948
phynos <phynos@126.com>
2021-03-25 16:54:07 +0800

    整体联调

--- solar-module/solar-rule-easyrules/src/test/java/com/phynos/solar/rule/easyrules/rule/IotRuleTest.java
+++ solar-module/solar-rule-easyrules/src/test/java/com/phynos/solar/rule/easyrules/rule/IotRuleTest.java
@@ -7,6 +7,7 @@ import org.jeasy.rules.api.Facts;
 import org.jeasy.rules.api.Rules;
 import org.jeasy.rules.api.RulesEngine;
 import org.jeasy.rules.core.DefaultRulesEngine;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -16,46 +17,46 @@ import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+/**
+ * 规则引擎-单元测试
+ */
 @DisplayName("规则引擎-IOT测试")
 @SpringBootTest(classes = IotRuleTest.class)
 class IotRuleTest {
 
-    @DisplayName("规则测试")
-    @Test
-    public void testIot() throws Exception {
+    static final Map<String, IotDevice> deviceMap = new HashMap<>();
+    static final RulesEngine rulesEngine = new DefaultRulesEngine();
+    static final Rules rules = new Rules();
+    static final Facts facts = new Facts();
+
+    @BeforeAll
+    public static void init() {
+        //初始化设备
+        IotDevice device1 = new JsonDeviceBuild("22R201512DB21188").fromResource("/product/temp.json").build();
+        deviceMap.put(device1.getSn(), device1);
+        IotDevice device2 = new JsonDeviceBuild("0102").fromResource("/product/temp.json").build();
+        deviceMap.put(device2.getSn(), device2);
+        //初始化规则
         IotRule openRule = new IotRuleBuild().fronJson("/ruler/open.json").build();
         assertNotNull(openRule);
         IotRule closeRule = new IotRuleBuild().fronJson("/ruler/close.json").build();
         assertNotNull(closeRule);
         //注册规则
-        Rules rules = new Rules();
         rules.register(openRule);
         rules.register(closeRule);
-
-        Facts facts = new Facts();
-        final Map<String, IotDevice> deviceMap = new HashMap<>();
-        IotDevice device1 = createDevice1("0101");
-        deviceMap.put(device1.getSn(), device1);
-        IotDevice device2 = createDevice2("0201");
-        deviceMap.put(device2.getSn(), device2);
+        //
         facts.put("deviceMap", deviceMap);
-        RulesEngine rulesEngine = new DefaultRulesEngine();
+    }
+
+    @DisplayName("规则测试")
+    @Test
+    public void testIot() throws Exception {
         for (int i = 0; i < 100; i++) {
             int t = RandomUtils.nextInt(0, 50);
-            deviceMap.get("0101").getAttrs().get("temp").setValue(t + "");
+            deviceMap.get("22R201512DB21188").getAttrs().get("temp").setValue(t + "");
             rulesEngine.fire(rules, facts);
             Thread.sleep(100);
         }
-    }
-
-    private static IotDevice createDevice1(String sn) {
-        IotDevice device = new JsonDeviceBuild(sn).fromResource("/product/temp.json").build();
-        return device;
-    }
-
-    private static IotDevice createDevice2(String sn) {
-        IotDevice device = new JsonDeviceBuild(sn).fromResource("/product/box.json").build();
-        return device;
     }
 
 }
