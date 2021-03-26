6a6952434a136698cfe56810d0e03df75880ee13
phynos <phynos@126.com>
2021-03-26 10:03:55 +0800

    整体联调

--- solar-module/solar-device/src/main/java/com/phynos/solar/codec/device/DefaultIotDevice.java
+++ solar-module/solar-device/src/main/java/com/phynos/solar/codec/device/DefaultIotDevice.java
@@ -8,6 +8,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.jexl3.*;
 import org.apache.commons.lang3.StringUtils;
 
+import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -30,8 +31,17 @@ public class DefaultIotDevice implements IotDevice {
     @Setter
     private DefaultIotProduct product;
 
+    /**
+     * 拥有表达式的属性缓存
+     */
     @JsonIgnore
-    private Map<String, IotAtrribute> exprAttrs;
+    private final Map<String, IotAtrribute> exprAttrCache = new HashMap<>();
+
+    /**
+     * jexl表达式预编译缓存
+     */
+    @JsonIgnore
+    private final Map<String, JexlExpression> jexlExprCache = new HashMap<>();
 
     @Getter
     @Setter
@@ -60,12 +70,7 @@ public class DefaultIotDevice implements IotDevice {
             log.warn("无法找到属性，key={}", key);
             return;
         }
-        if (StringUtils.isNotEmpty(atrribute.getExpr())) {
-            jexlContext.set(key, value);
-            JexlExpression e = jexl.createExpression(atrribute.getExpr());
-            Object o = e.evaluate(jexlContext);
-            log.debug("表达式结果：{}", o);
-        } else {
+        if (StringUtils.isEmpty(atrribute.getExpr())) {
             atrribute.setValue(value);
         }
         calcExpr();
@@ -73,15 +78,27 @@ public class DefaultIotDevice implements IotDevice {
 
     @Override
     public void finishInit() {
-        //初始化表达式属性
-        exprAttrs = new HashMap<>();
         //初始化表达式上下文
-        getAttrs().forEach((k, v) -> {
-            jexlContext.set(k, v);
-            if (StringUtils.isNotEmpty(v.getExpr())) {
-                exprAttrs.put(k, v);
+        getAttrs().forEach((k, attribute) -> {
+            jexlContext.set(k, attribute);
+            if (StringUtils.isNotEmpty(attribute.getExpr())) {
+                //缓存有表达式的属性
+                exprAttrCache.put(k, attribute);
+                //预编译表达式
+                JexlExpression expr = jexl.createExpression(attribute.getExpr());
+                jexlExprCache.put(k, expr);
             }
         });
+    }
+
+    @Override
+    public boolean getOnline() {
+        return false;
+    }
+
+    @Override
+    public LocalDateTime getLastDataTime() {
+        return LocalDateTime.now();
     }
 
     @Override
@@ -109,11 +126,14 @@ public class DefaultIotDevice implements IotDevice {
         return product.getServices();
     }
 
+    /**
+     * 计算所有的 表达式属性
+     */
     private void calcExpr() {
-        exprAttrs.forEach((k, v) -> {
-            JexlExpression e = jexl.createExpression(v.getExpr());
+        exprAttrCache.forEach((k, v) -> {
+            JexlExpression e = jexlExprCache.get(k);//直接从缓存获取预编译的表达式
             Object o = e.evaluate(jexlContext);
-            log.debug("表达式结果：{}", o);
+            log.debug("信号名称：{}，表达式：{}，结果：{}", v.getName(), v.getExpr(), o);
             v.setValue(o.toString());
         });
     }
--- solar-module/solar-device/src/main/java/com/phynos/solar/codec/device/IotDevice.java
+++ solar-module/solar-device/src/main/java/com/phynos/solar/codec/device/IotDevice.java
@@ -2,6 +2,8 @@ package com.phynos.solar.codec.device;
 
 import com.phynos.solar.codec.product.IotProduct;
 
+import java.time.LocalDateTime;
+
 /**
  * IOT设备接口
  *
@@ -19,5 +21,17 @@ public interface IotDevice extends IotProduct {
      * 完成设备相关初始化
      */
     void finishInit();
+
+    /**
+     * 是否在线
+     * @return
+     */
+    boolean getOnline();
+
+    /**
+     * 最后一次数据更新时间
+     * @return
+     */
+    LocalDateTime getLastDataTime();
 
 }
